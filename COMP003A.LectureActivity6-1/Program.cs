94db1ebe08ef1f062cf9fa2fcbd21b152b6c2544
briannaordaz <briannaordaz5@gmail.com>
2025-02-18 22:54:55 -0800

    created instances of 'Vehicle' and 'Car' in the 'Main' Class, and called 'DispayInfo()' on each object

---
+++ COMP003A.LectureActivity6-1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace COMP003A.LectureActivity6_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create a Vehicle object with brand and year
+        Vehicle myVehicle = new Vehicle { Brand = "Toyota", Year = 2020 };
+        // Create a Car object with brand, year, and number of doors
+        Car myCar = new Car("Honda", 2022, 4);
+        
+        // Display vehicle details
+        myVehicle.DisplayInfo();
+        // Display car details
+        myCar.DisplayInfo();
+    }
+}
